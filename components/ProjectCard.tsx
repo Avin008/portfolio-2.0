2730053b30ec90acf204144d2246bff70f77d428
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-05-30 19:47:43 +0530

    removed gifs

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { useState } from "react";
 import { ProjectsDataType } from "../types";
 import Image from "next/image";
 
@@ -8,26 +6,14 @@ export const ProjectCard = ({
 }: {
   projectData: ProjectsDataType;
 }): React.ReactElement => {
-  const [animate, setAnimate] = useState<boolean>(false);
-
-  const toggleAnimate = (value: boolean) => {
-    setAnimate(value);
-  };
-
   return (
     <a href={projectData.githubLink} target="_blank">
-      <div
-        onMouseEnter={() => toggleAnimate(true)}
-        onMouseLeave={() => toggleAnimate(false)}
-        className="border border-gray-600 hover:shadow-[#13141b] hover:shadow-md hover:cursor-pointer transition-all h-fit shadow-md rounded-md"
-      >
+      <div className="border border-gray-600 hover:shadow-[#13141b] hover:shadow-md hover:cursor-pointer transition-all h-fit shadow-md rounded-md">
         <div className="bg-transparent h-36 rounded-t-md relative">
           <Image
             loading="lazy"
             className="aspect-video transition-all rounded-t-md object-fill"
-            src={
-              animate ? projectData.gif : projectData.img
-            }
+            src={projectData.img}
             alt=""
             fill
           />
