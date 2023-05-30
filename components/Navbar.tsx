1300217ea6b3d231e814fb2ce278160fa2bf59c2
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-05-30 18:30:39 +0530

    removed nav icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,9 +10,8 @@ const socialLinks = {
 export const Navbar = (): React.ReactElement => {
   return (
     <nav className="fixed w-[60%] z-50 mx-auto left-0 right-0 h-14 flex justify-between items-center">
-      <div className="">
+      <div>
         <span></span>
-        <span className="text-xl">✨</span>
       </div>
       <ul className="flex gap-8 bg-background bg-opacity-60 items-center border shadow-md border-gray-700 py-2 px-4 rounded-full text-gray-300 text-lg">
         <li className="hover:text-gray-50 transition-all">
